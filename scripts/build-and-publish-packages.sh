3ae0a86ab8f0ce930bec750d6edda2c5cfb3ab21
Olakunle Olaniyi <rugging24@gmail.com>
2021-12-14 13:36:55 +0100

    doc build imports

--- scripts/build-and-publish-packages.sh
+++ scripts/build-and-publish-packages.sh
@@ -7,6 +7,8 @@ pip install mkdocs-material mkdocs-git-revision-date-plugin \
     mkdocs-material mkdocs-material-extensions mkdocstrings \
     mkdocs-autorefs mkdocs markdown markupsafe twine wheel
 
+python -c 'import sys, os; sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))'
+
 python setup.py sdist bdist_wheel
 twine check dist/*
 mkdocs build
