124c87d7a22ba902f559c7d276a2076f6710bf46
Olakunle Olaniyi <rugging24@gmail.com>
2021-12-18 02:40:38 +0100

    building docs

--- scripts/build-and-publish-packages.sh
+++ scripts/build-and-publish-packages.sh
@@ -7,11 +7,11 @@ pip install mkdocs-material mkdocs-git-revision-date-plugin \
     mkdocs-material mkdocs-material-extensions mkdocstrings \
     mkdocs-autorefs mkdocs markdown markupsafe twine wheel
 
-python -c 'import sys, os; sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("."), "..")))'
+# python -c 'import sys, os; sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("."), "..")))'
 
 python setup.py sdist bdist_wheel
 twine check dist/*
-mkdocs build
+# mkdocs build
 
 twine upload dist/*
-mkdocs gh-deploy --force
+# mkdocs gh-deploy --force
