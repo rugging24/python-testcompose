85b2d186517809ccac0fb87053f6353078f6d0f8
Github Action CI User <rugging24@gmail.com>
2022-12-07 11:18:53 +0100

    version creation with tags corrected

--- scripts/create_version.sh
+++ scripts/create_version.sh
@@ -4,13 +4,13 @@ which git || exit 1
 
 function compute_version() {
     git fetch --tags
-    current_version="0.1.0" #$(git describe --tags --abbrev=0)
+    current_version=$(git describe --tags --abbrev=0)
     if [ ! -z "${current_version}" ]; then
         major_version=$(echo $current_version | awk '{split($0,a,"."); print a[1]}')
         minor_version=$(echo $current_version | awk '{split($0,a,"."); print a[2]}')
         update_version=$(echo $current_version | awk '{split($0,a,"."); print a[3]}')
 
-        if [ $((${update_version} + 1)) -ge 11 ]; then
+        if [ $(($update_version + 1)) -ge 11 ]; then
             update_version=0
             minor_version=$((${minor_version} + 1))
         else
@@ -25,10 +25,10 @@ function compute_version() {
 
     latest_version="${major_version}.${minor_version}.${update_version}"
 
-    # git config user.email "rugging24@gmail.com"
-    # git config user.name "Github Action CI User"
-    # git tag -a "${latest_version}" -m "creating ${latest_version} tag version"
-    # git push origin tag "${latest_version}"
+    git config user.email "rugging24@gmail.com"
+    git config user.name "Github Action CI User"
+    git tag -a "${latest_version}" -m "creating ${latest_version} tag version"
+    git push origin tag "${latest_version}"
 
     echo ${latest_version}
 }
